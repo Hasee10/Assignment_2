f73897837c329601af60bbb2c13b3aacfb236f68
Hasee10 <ihaseebarshad10@gmail.com>
2024-10-20 02:13:26 +0000

    Made the first node consisiting of tthe 4 nodes that were required

--- i23-2578.cpp
+++ i23-2578.cpp
@@ -11,6 +11,33 @@ Assignment # 2
 #include <ncurses.h>
 using namespace std;
 
+struct Node 
+{
+    char data;
+    Node* up;//upper node 
+    Node* down;//lower node
+    Node* left;//left node
+    Node* right;//right node
+
+    Node()
+    {
+         data = 0;
+         up = nullptr;
+         down = nullptr;
+         left = nullptr;
+         right = nullptr;
+    }
+
+    Node(char ch)
+    {
+         data = ch;
+         up = nullptr;
+         down = nullptr;
+         left = nullptr;
+         right = nullptr;
+    }
+};
+
 
 
 int main()
